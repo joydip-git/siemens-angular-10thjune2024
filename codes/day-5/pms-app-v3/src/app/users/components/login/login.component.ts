a41e54d712bbf9460439f367ffa41c9c41c6b4f1
Joydip <joydip.mondal@gmail.com>
2024-06-14 16:03:13 +0530

    login update

--- codes/day-5/pms-app-v3/src/app/users/components/login/login.component.ts
+++ codes/day-5/pms-app-v3/src/app/users/components/login/login.component.ts
@@ -1,25 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { passwordValidator } from '../../../validators/password-validator';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy {
 
   // loginFrm = new FormGroup({
   //   username: new FormControl('',[Validators.required, Validators.email]),
   //   password: new FormControl('',Validators.required)
   // })
   loginFrm: FormGroup;
-  constructor(private builder: FormBuilder) {
+  private loginSub?: Subscription;
+
+  constructor(
+    private builder: FormBuilder,
+    private userSvc: UserService,
+    private router: Router,
+    private currentRoute: ActivatedRoute
+  ) {
 
     this.loginFrm = this.builder.group({
       username: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, passwordValidator]]
     })
+  }
+  ngOnDestroy(): void {
+    this.loginSub?.unsubscribe()
   }
 
   get username() {
@@ -29,8 +43,32 @@ export class LoginComponent {
     return this.loginFrm.get('password')
   }
   submit() {
-    //console.log(this.loginFrm.value);
-    console.log(this.loginFrm);
+    const user = <User>this.loginFrm.value
+    if (window.confirm('want to submit?')) {
+      this.loginSub = this.userSvc.login(user).subscribe({
+        next: (response) => {
+          if (response.data !== null) {
+            window.alert(response.message)
+            //save the token
+            sessionStorage.setItem('token', response.data)
+          } else
+            window.alert(response.message)
+        },
+        error: (err) => {
+          window.alert(err.message)
+        },
+        complete: () => {
+          //redirect
+          const snapshot = this.currentRoute.snapshot
+          const returnUrl = snapshot.queryParams["returnUrl"]
+          if (returnUrl && returnUrl !== '') {
+            this.router.navigate([returnUrl])
+          } else {
+            this.router.navigate(['/products'])
+          }
+        }
+      })
+    }
   }
 
 }
